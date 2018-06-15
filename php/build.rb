de2141a8a6ddcd8b4bec9aca1019ff60891a9496
renat <renat@iron.io>
2018-06-15 19:19:37 +0600

    Updated php image to 7.1.17.

--- php/build.rb
+++ php/build.rb
@@ -8,12 +8,14 @@ build("#{name}:#{tag}")
 v, status = Open3.capture2e("docker run --rm #{name}:#{tag} php -v")
 v = v.strip
 puts v
-# returns: PHP 5.6.17 (cli) (built: Jan 24 2016 22:24:02) \nCopyright (c) 1997-2015 The PHP Group\nZend Engine v2.6.0, Copyright (c) 1998-2015 Zend Technologies
+# returns: PHP 7.1.17 (cli) (built: May  3 2018 17:39:19) ( NTS ) \nCopyright (c) 1997-2018 The PHP Group\nZend Engine v3.1.0, Copyright (c) 1998-2018 Zend Technologies
 v = v.split(' ')[1]
 puts v
-vtag(name, tag, v, false)
+new_tags = vtag(name, tag, v, false)
 
 Dir.chdir 'dev'
 tag = "dev"
 build("#{name}:#{tag}")
-vtag(name, tag, v, true)
+new_tags += vtag(name, tag, v, true)
+
+push_all(name, new_tags)
